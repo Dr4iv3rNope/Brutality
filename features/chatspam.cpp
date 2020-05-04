20fa06d3059753a90807c979cbf2599b9a13f19a
Dr4iv3r <dr4iv3r2004@gmail.com>
2020-05-04 20:50:57 +0300

    Added chat spammer (TODO)

---
+++ features/chatspam.cpp
@@ -0,0 +1,12 @@
+#include "chatspam.hpp"
+
+#include "../imgui/imgui.h"
+
+void Features::ChatSpam::Think() noexcept
+{
+}
+
+void Features::ChatSpam::RegisterWindow() noexcept
+{
+
+}
---
+++ features/chatspam.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Features
+{
+	namespace ChatSpam
+	{
+		extern void Think() noexcept;
+
+		extern void RegisterWindow() noexcept;
+	}
+}
