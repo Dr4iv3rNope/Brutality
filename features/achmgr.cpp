f6ff8367e6e377f7769a2ac019f234b22941954d
Dr4iv3r <dr4iv3r2004@gmail.com>
2020-05-14 01:25:37 +0300

    Added more features to achievement manager

--- features/achmgr.cpp
+++ features/achmgr.cpp
@@ -3,6 +3,7 @@
 #include "../imgui/imgui.h"
 #include "../imgui/custom/windowmanager.hpp"
 #include "../imgui/custom/errors.hpp"
+#include "../imgui/custom/text.hpp"
 
 #include "../util/strings.hpp"
 #include "../util/xorstr.hpp"
@@ -12,17 +13,19 @@
 
 #include "../sourcesdk/achievementmgr.hpp"
 #include "../sourcesdk/engineclient.hpp"
+#include "../sourcesdk/localize.hpp"
 
 #include "../main.hpp"
 
-#include <map>
+#include <deque>
 #include <string>
 
 enum Err_
 {
 	Err_None = -1,
 	Err_FailedGetMgr,
-	Err_FailedGetAch
+	Err_FailedGetAch,
+	Err_FailedGetLocal
 };
 
 static Err_ currentError { Err_None };
@@ -38,9 +41,82 @@ static ImGui::Custom::ErrorList errorList
 		ImVec4(1.f, 1.f, 0.f, 1.f),
 		UTIL_SXOR("Check logs for more information")
 	),
+
+	ImGui::Custom::Error(
+		UTIL_SXOR("Failed to get some localized achievement name/description"),
+		ImVec4(1.f, 1.f, 0.f, 1.f),
+		UTIL_SXOR("Check logs for more information")
+	),
 };
 
-static std::map<std::string, SourceSDK::Achievement*> achievementList;
+struct AchievementInfo
+{
+private:
+	inline void SetupName() noexcept
+	{
+		const auto name_local_format = UTIL_FORMAT(
+			'#' << achievement->name << UTIL_SXOR("_NAME")
+		);
+
+		const auto name_local = interfaces->localize->Find(name_local_format.c_str());
+
+		if (name_local)
+			name = Util::ToMultiByte(name_local);
+		else
+		{
+			UTIL_LOG(UTIL_WFORMAT(
+				UTIL_XOR("Failed to get localized achievement name. ID: ") <<
+				achievement->achievementID <<
+				UTIL_XOR(". Name: ") <<
+				achievement->name
+			));
+
+			currentError = Err_FailedGetLocal;
+
+			name = achievement->name;
+		}
+	}
+
+	inline void SetupDesc() noexcept
+	{
+		const auto desc_local_format = UTIL_FORMAT(
+			'#' << achievement->name << UTIL_SXOR("_DESC")
+		);
+
+		const auto desc_local = interfaces->localize->Find(desc_local_format.c_str());
+
+		if (desc_local)
+			desc = Util::ToMultiByte(desc_local);
+		else
+		{
+			UTIL_LOG(UTIL_WFORMAT(
+				UTIL_XOR("Failed to get localized achievement desctiption. ID: ") <<
+				achievement->achievementID <<
+				UTIL_XOR(". Name: ") <<
+				achievement->name
+			));
+
+			currentError = Err_FailedGetLocal;
+		}
+	}
+
+public:
+	std::string name, desc;
+	SourceSDK::Achievement* achievement;
+
+	inline AchievementInfo(SourceSDK::Achievement* achievement) noexcept
+		: achievement { achievement }
+	{
+		UTIL_DEBUG_ASSERT(achievement);
+
+		SetupName();
+		SetupDesc();
+	}
+
+	inline auto* operator->() noexcept { return achievement; }
+};
+
+static std::deque<AchievementInfo> achievementList;
 static int currentAchievement = -1;
 static auto currentAchievementIter = achievementList.end();
 
@@ -57,18 +133,19 @@ static void UpdateAchievementList() noexcept
 		for (auto i = 0; i < ach_mgr->GetAchievementCount(); i++)
 		{
 			if (auto ach = ach_mgr->GetAchievementByIndex(i))
-				achievementList.insert(std::make_pair(ach->name, ach));
+				achievementList.push_back(AchievementInfo(ach));
 			else
 			{
 				currentError = Err_FailedGetAch;
 
 				UTIL_LOG(UTIL_WFORMAT(
-					UTIL_XOR("Failed to get achievement by index ") << i
+					UTIL_XOR("Failed to get achievement at index ") << i
 				));
 			}
 		}
 
-		if (currentError != Err_FailedGetAch)
+		if (currentError != Err_FailedGetAch &&
+			currentError != Err_FailedGetLocal)
 			currentError = Err_None;
 
 		UTIL_LABEL_OK();
@@ -77,21 +154,21 @@ static void UpdateAchievementList() noexcept
 	{
 		currentError = Err_FailedGetMgr;
 
-		UTIL_XLOG(L"Failed to get achievement manger!");
+		UTIL_XLOG(L"Failed to get achievement manager!");
 		UTIL_LABEL_OVERRIDE();
 	}
 }
 
 static void UnlockAll() noexcept
 {
-	for (auto& ach : achievementList)
-		ach.second->achieved = true;
+	for (auto& info : achievementList)
+		info->achieved = true;
 }
 
 static void LockAll() noexcept
 {
-	for (auto& ach : achievementList)
-		ach.second->achieved = false;
+	for (auto& info : achievementList)
+		info->achieved = false;
 }
 
 static bool UnlockCurrent() noexcept
@@ -99,9 +176,9 @@ static bool UnlockCurrent() noexcept
 	if (currentAchievement == -1)
 		return false;
 
-	UTIL_DEBUG_ASSERT(currentAchievementIter == achievementList.end());
+	UTIL_DEBUG_ASSERT(currentAchievementIter != achievementList.end());
 
-	currentAchievementIter->second->achieved = true;
+	currentAchievementIter->achievement->achieved = true;
 	return true;
 }
 
@@ -110,9 +187,9 @@ static bool LockCurrent() noexcept
 	if (currentAchievement == -1)
 		return false;
 
-	UTIL_DEBUG_ASSERT(currentAchievementIter == achievementList.end());
+	UTIL_DEBUG_ASSERT(currentAchievementIter != achievementList.end());
 
-	currentAchievementIter->second->achieved = false;
+	currentAchievementIter->achievement->achieved = false;
 	return true;
 }
 
@@ -139,7 +216,7 @@ static void DrawWindow(ImGui::Custom::Window&) noexcept
 		auto iter = achievementList.begin();
 		std::advance(iter, idx);
 
-		*out = iter->first.c_str();
+		*out = iter->name.c_str();
 		return true;
 	}, nullptr, achievementList.size(), 10))
 	{
@@ -149,12 +226,23 @@ static void DrawWindow(ImGui::Custom::Window&) noexcept
 
 	if (currentAchievement != -1)
 	{
-		UTIL_DEBUG_ASSERT(currentAchievementIter == achievementList.end());
+		UTIL_DEBUG_ASSERT(currentAchievementIter != achievementList.end());
 
-		ImGui::Text(UTIL_CXOR("Current Achievement: (%i) %s"),
-					currentAchievement,
-					currentAchievementIter->first.c_str()
+		ImGui::Custom::CopiableText(UTIL_CXOR("Current Achievement: %i"), currentAchievement);
+		ImGui::Custom::CopiableText(UTIL_CXOR("Name: %s"), currentAchievementIter->name.c_str());
+		ImGui::Custom::CopiableText(UTIL_CXOR("Description: %s"), currentAchievementIter->desc.c_str());
+
+		ImGui::Custom::CopiableText(
+			UTIL_CXOR("Is Achieved?: %s"),
+			ImGui::Custom::ToTextBoolean(currentAchievementIter->achievement->achieved).c_str()
 		);
+
+		ImGui::Custom::CopiableText(
+			UTIL_CXOR("Hidden?: %s"),
+			ImGui::Custom::ToTextBoolean(currentAchievementIter->achievement->hideUntilAchieved).c_str()
+		);
+
+		ImGui::NewLine();
 
 		if (ImGui::Button(UTIL_CXOR("Unlock")))
 			UnlockCurrent();
