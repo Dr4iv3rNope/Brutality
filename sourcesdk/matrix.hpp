1e3f80e589a50b24b753def5f1c80879bc84f226
Dr4iv3r <dr4iv3r2004@gmail.com>
2020-05-16 15:49:26 +0300

    Using std::array for matrix

--- sourcesdk/matrix.hpp
+++ sourcesdk/matrix.hpp
@@ -1,13 +1,14 @@
 #pragma once
+#include <array>
 
 namespace SourceSDK
 {
-	template <unsigned X, unsigned Y>
-	using Matrix = float[X][Y];
+	template <typename T, std::size_t X, std::size_t Y>
+	using Matrix = std::array<std::array<T, Y>, X>;
 
-	using Matrix3x3 = Matrix<3, 3>;
-	using Matrix3x4 = Matrix<3, 4>;
-	using Matrix4x4 = Matrix<4, 4>;
+	using Matrix3x3 = Matrix<float, 3, 3>;
+	using Matrix3x4 = Matrix<float, 3, 4>;
+	using Matrix4x4 = Matrix<float, 4, 4>;
 
 	using VMatrix = Matrix4x4;
 }
